38b33c091e3d41e0ffeea901d6d1f7b349a7d0b8
Nick Gamroth <thebeekeeper@gmail.com>
2013-06-06 20:28:41 -0500

    added sonar properties file

---
+++ Laurus.TaskBoss.Core.UnitTest/FakeTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Laurus.TaskBoss.Core.UnitTest
+{
+    public class FakeTests
+    {
+        [Fact]
+        public void FakeTest1()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void FakeTest2()
+        {
+            Assert.True(false);
+        }
+
+        [Fact]
+        public void FakeTest3()
+        {
+            Assert.Equal(9, 9);
+        }
+    }
+}
