6daa3f609fde40c1a351a19be54f18daef87ddba
Nick Gamroth <thebeekeeper@gmail.com>
2013-10-02 16:01:59 -0500

    redid conflicts

--- Laurus.TaskBoss.Core/WindowsExeJob.cs
+++ Laurus.TaskBoss.Core/WindowsExeJob.cs
@@ -5,23 +5,14 @@ using System;
 
 namespace Laurus.TaskBoss.Core
 {
-<<<<<<< HEAD
 	public class WindowsExeJob : IJob
 	{
 		public WindowsExeJob()
 		{
+			// not injecting this becuase it gets created by quartz - could use the quartz facility, but i don't like 
+			// that you have to use the xml config
 			_log = new Logger();
 		}
-=======
-    public class WindowsExeJob : IJob
-    {
-        public WindowsExeJob()
-        {
-            // not injecting this becuase it gets created by quartz - could use the quartz facility, but i don't like 
-            // that you have to use the xml config
-            _log = new Logger();
-        }
->>>>>>> 43d92b6885c556c8331b71e12ea027da2c7bf59e
 
 		public void Execute(IJobExecutionContext context)
 		{
@@ -41,23 +32,17 @@ namespace Laurus.TaskBoss.Core
 			var wdir = context.MergedJobDataMap.Get("working_dir") as string;
 			var startInfo = new ProcessStartInfo(cmd);
 			startInfo.Arguments = args;
-<<<<<<< HEAD
 			startInfo.WorkingDirectory = wdir;
-			var startedProcess = System.Diagnostics.Process.Start(startInfo);
+			try
+			{
+				var startedProcess = System.Diagnostics.Process.Start(startInfo);
+			}
+			catch (Exception e)
+			{
+				_log.Error("Exception caught while starting {0}: {1}", exe, e.Message);
+				_log.Error("Working directory: {0}", wdir);
+			}
 		}
-=======
-            startInfo.WorkingDirectory = wdir;
-            try
-            {
-                var startedProcess = System.Diagnostics.Process.Start(startInfo);
-            }
-            catch (Exception e)
-            {
-                _log.Error("Exception caught while starting {0}: {1}", exe, e.Message);
-                _log.Error("Working directory: {0}", wdir);
-            }
-        }
->>>>>>> 43d92b6885c556c8331b71e12ea027da2c7bf59e
 
 		private ILog _log;
 	}
